fc5060c766973f4433aa395e0b29710e91f116aa
PCarerTracy <pcartertracy@smu.edu>
2014-09-12 11:19:53 -0500

    trying to fix validateForm logic

--- inclass/jscript.js
+++ inclass/jscript.js
@@ -1,34 +1,41 @@
 
 <script type="text/javascript" language="javascript">
-// A form validation function template.
-function validateForm(event) {
-  alert('Form is being submitted. Delete this line from the js.');
+function validateForm(event) 
   event.preventDefault(); // don't submit the form, because we're going to work with data on the client only.
+
   var formIsValid = true;
-  // TODO
-  // Inspect the values of each input element to make sure they are not blank.
-  // If any of the fields are blank, formIsValid should be set to false.
-  // AND the error message node in the dom should describe the errors.
-  // eg: First name is blank. Favorite food is blank.
+
+  var x = document.forms["myForm"]["fname"].value;
+    if (x==null || x=="") {
+        alert("First name must be filled out");
+        formIsValid = false;
+
+  var y = document.forms["myForm"]["lname"].value;
+    if (y==null || y=="") {
+        alert("Last name must be filled out");
+        formIsValid = false;
+
+  var z = document.forms["myForm"]["fav_food"].value;
+    if (z==null || z=="") {
+        alert("Favorite food must be filled out");
+        formIsValid = false;
+
   if (!formIsValid) {
-    // TODO update the error div and display it.
+      document.getElementById("lname").innerHTML = "First name must be filled out.";
+      document.getElementById("fname").innerHTML = "Last name must be filled out.";
+      document.getElementById("fav_food").innerHTML = "Favorite food must be filled out.";
   } else {
-    // TODO update the h1 span contents. Hint: see element.innerHTML
+    function update() {
+      document.getElementById("fullName").innerHTML = document.getElementById("fname").innerHTML;
+      document.getElementById("fillName").innerHTML = document.getElementById("lname").innerHTML ;
+      document.getElementById("favoriteFood").innerHTML = document.getElementById("fav_food").innerHTML;
+    }
   }
 }
 
-// "Only once the document is loaded, execute the following function."
+
 window.onload = function(event) {
-  alert('Window (and document) has loaded. Delete this line from the js.');
-  // Get the form element, so we can manipulate it.
   var formNode = document.getElementById("myForm");
-  // When the form is submitted, execute the following function.
   formNode.addEventListener("submit", validateForm, false);
-  // Notice how the identifier `validateForm` itself is passed, not the
-  // return value from calling the `validateForm` function.
-  // eg. validateForm vs validateForm().
-  // We are literally passing the `addEventListener` method a function.
-  // In JavaScript, and other functional langs,
-  // functions are "first-class citizens".
 }
 </script>
