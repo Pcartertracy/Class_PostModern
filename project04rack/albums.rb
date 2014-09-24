6e56857383a09d3dbd2f19e80d102681476dbacf
PCarerTracy <pcartertracy@smu.edu>
2014-09-24 13:58:24 -0500

    Project 4 displaying form and list.

---
+++ project04rack/albums.rb
@@ -0,0 +1,32 @@
+require 'rack'
+
+class AlbumApp 
+	def call(env)
+		request = Rack::Request.new(env)
+		case request.path 
+		when "/form" then render_form(request)
+		when "/list" then render_list(request) 
+		else render_404 
+		end
+	end
+ 
+	def render_form(request) 
+		response = Rack::Response.new 
+		File.open("form.html", "rb") { |form| response.write(form.read)} 
+		(1..100).each { |i| response.write("<option value=\"#{i}\">#{i}</option>\n")}
+		File.open("formBottem.html", "rb") { |form| response.write(form.read)}
+		response.finish 
+	end 
+
+	def render_list(request) 
+		response = Rack::Response.new 
+		File.open("top_100_albums.txt", "rb") { |file| response.write(file.read)} 
+		response.finish 
+	end 
+
+	def render_404 
+		[404, {"Content-Type" => "text/plain"},["nothing here!"]]
+	end 
+end 
+
+Rack::Handler::WEBrick.run AlbumApp.new, :Port => 8080 
