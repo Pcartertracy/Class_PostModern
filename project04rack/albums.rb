a1805b417ad1d40e0af2bcf007ea421bd080c239
PCarerTracy <pcartertracy@smu.edu>
2014-10-05 18:22:17 -0500

    impimenting Sqlite DB

--- project04rack/albums.rb
+++ project04rack/albums.rb
@@ -1,9 +1,9 @@
 require 'rack'
+require 'sqlite3'
 
 class Album
 	attr_accessor :rank,:title,:year
-
-	def init(rank, str)
+	def initialize(rank, title, year)
 		@rank = rank 
 		@title = title
 		@year = year
@@ -22,45 +22,46 @@ class AlbumApp
  
 	def render_form(request) 
 		response = Rack::Response.new 
-		File.open("form.html", "rb") { |form| response.write(form.read)} 
-		(1..100).each { |i| response.write("<option value=\"#{i}\">#{i}</option>\n")}
-		File.open("formBottem.html", "rb") { |form| response.write(form.read)}
+	    response.write(ERB.new(File.read("form.html.erb")).result(binding))  
 		response.finish 
 	end 
 
 	def render_list(request) 
 		response = Rack::Response.new 
+
 		data = []
-		File.open("top_100_albums.txt", "rb") do |f|
-  			f.each_line do |line|
-  				line.chomp!
-    			data << line.split(", ")
+		#SQLITE
+		SQLite3::Database.open( "albums.sqlite3.db" ) do |db|
+  			db.execute( "SELECT * FROM albums" ) do |row|
+   				 puts row
+   				 data << row
   			end
 		end
-		songs = []
+
+		#File.open("top_100_albums.txt", "rb") do |f|
+  		#	f.each_line do |line|
+  		#		line.chomp!
+    	#		data << line.split(", ")
+  		#	end 
+		#end #--> 
+
+		#maps
+		albums = []
+
 		data.each_with_index do |a, i|
-			songs << [i + 1, a[0], a[1]]
+		   albums << Album.new( a[0], a[1], a[2])  # might have to change per SQLite 
 		end
 
 		if (request.params["order"] == "name")
-			songs.sort! do |l, r| 
-				l[1] <=> r[1]
+			albums.sort! do |l, r| 
+				l.title <=> r.title
 			end	
 		elsif (request.params["order"] == "year")
-			songs.sort! do |l, r| 
-				l[2] <=> r[2]
+			albums.sort! do |l, r| 
+				l.year <=> r.year
 			end
 		end
-
-		File.open("list.html", "rb") { |list| response.write(list.read)} 
-		songs.each do |song|
-			response.write("\t<tr class=\"selected\">\n" : "\t<tr>\n")
-			response.write("<tr>\n<td>#{song[0]}</td><td>#{song[1]}</td><td>#{song[2]}</td>\n</tr>\n")
-
-
-			/response.write("<tr>\n<td>#{album.rank]}</td><td>#{album.name}</td><td>#{album.year}</td>\n</tr>\n")/
-		end
-		File.open("listBottem.html", "rb") { |list| response.write(list.read)}
+		response.write(ERB.new(File.read("list.html.erb")).result(binding))
 		response.finish 
 	end 
 
