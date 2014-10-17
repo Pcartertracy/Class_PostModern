9b367ccacd4a435332e1add9ab931886f6ce6aef
PCarerTracy <pcartertracy@smu.edu>
2014-10-17 10:02:21 -0500

    Amending/impliment rails scafolding

--- project04.2/albums.rb
+++ project04.2/albums.rb
@@ -6,6 +6,14 @@ DataMapper.setup(:default, "sqlite3://#{Dir.pwd}/albums.sqlite3.db")
 
 set :port, 8080
 
-get "/" do
-  "Sinatra is working!"
-end
+get "/form" do 
+	erb :form
+end 
+
+post "/list" do 
+	@sort_order =  params[:order]
+	@selected = params[:rank] 
+	@albums = Album.all(:order => @sort_order.to_sym)
+	erb :list 
+end 
+
