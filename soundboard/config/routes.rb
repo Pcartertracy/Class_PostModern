c3daf098c242c01ad8ddb536b25e3b8183fca3d2
PCarerTracy <pcartertracy@smu.edu>
2014-10-22 11:37:29 -0500

    failed attempt to impliment comments under SoundBites

--- soundboard/config/routes.rb
+++ soundboard/config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  resources :posts
+  
+  resources :sound_bites do 
+    resources :posts
+  end
+
   resources :sound_bites
 
   # The priority is based upon order of creation: first created -> highest priority.
--- soundboard/db/schema.rb
+++ soundboard/db/schema.rb
@@ -11,7 +11,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141017145646) do
+ActiveRecord::Schema.define(version: 20141022153218) do
+
+  create_table "comments", force: true do |t|
+    t.string   "body"
+    t.integer  "sound_bite_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "posts", force: true do |t|
+    t.integer  "sb_id"
+    t.text     "body"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "posts", ["sb_id"], name: "index_posts_on_sb_id"
 
   create_table "sound_bites", force: true do |t|
     t.string   "title"
